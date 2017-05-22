58d481db324fceb6ee5c40bd86c9875256403fb5
jagathprasanga <jagathprasanga@gmail.com>
2017-05-22 07:19:59 +0530

    IsValidJobNo methord added

--- app/src/main/java/lk/steps/breakdownassist/ReadSMS.java
+++ app/src/main/java/lk/steps/breakdownassist/ReadSMS.java
@@ -16,108 +16,96 @@ import java.util.regex.Pattern;
 /**
  * Created by Chirantha on 15/10/2016.
  */
-public class ReadSMS
-{
+public class ReadSMS {
     /* TODO : Handle the empty inbox case when a new SMS receive error*/
 
-    public static String getNextID(Context context)
-    {
-        String[] reqCols = new String[] { "_id"};
-        Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/inbox"), reqCols,  null, null, null);
+    public static String getNextID(Context context) {
+        String[] reqCols = new String[]{"_id"};
+        Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/inbox"), reqCols, null, null, null);
 
-        String returnval="1";
-        if (cursor.moveToFirst())
-        { // must check the result to prevent exception
-            returnval=String.valueOf((Long.parseLong( cursor.getString(0))+1));
-        }
-        else
-        {
+        String returnval = "1";
+        if (cursor.moveToFirst()) { // must check the result to prevent exception
+            returnval = String.valueOf((Long.parseLong(cursor.getString(0)) + 1));
+        } else {
             // empty box, no SMS
-            returnval="1";
+            returnval = "1";
         }
 
         cursor.close();
         return returnval;
     }
 
-    public static void SyncSMSInbox(Context context)
-    {
+    public static void SyncSMSInbox(Context context) {
         ProgressDialog progress;
         progress = ProgressDialog.show(context, "dialog title",
                 "dialog message", true);
 
         // List required columns
 
-        String[] reqCols = new String[] { "_id", "address","date_sent", "body"};
+        String[] reqCols = new String[]{"_id", "address", "date_sent", "body"};
         Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/inbox"), reqCols, null, null, null);
 
-        if (cursor.moveToFirst())
-        { // must check the result to prevent exception
-            do
-            {
+        if (cursor.moveToFirst()) { // must check the result to prevent exception
+            do {
                 String sID = cursor.getString(0);
                 String sAddress = cursor.getString(1);
-                String sFullMessage=cursor.getString(3);
-                Date callDayTime = new Date(Long.parseLong( cursor.getString(2)));
+                String sFullMessage = cursor.getString(3);
+                Date callDayTime = new Date(Long.parseLong(cursor.getString(2)));
                 SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss a");
                 String time = timeFormat.format(callDayTime);
                 //String sNextID =getNextID(context);
-                String sJob_No=extractJobNo(sFullMessage);
-                String sAcct_num=extractAccountNo(sFullMessage);
-                String sPhone_No=extractPhoneNo(sFullMessage);
+                String sJob_No = extractJobNo(sFullMessage);
+                String sAcct_num = extractAccountNo(sFullMessage);
+                String sPhone_No = extractPhoneNo(sFullMessage);
 
-                if(sJob_No != null) {// Added on 2017/05/22 to prevent irrelevant sms to add as a breakdown
-                    if (sJob_No.length() == 21) {
-                        DBHandler dbHandler = new DBHandler(context, null, null, 1);
-                        dbHandler.addBreakdown(sID, time, sAcct_num, sFullMessage, sJob_No, sPhone_No, sAddress);
-                        dbHandler.close();
-                    }
+                if (IsValidJobNo(sJob_No)) {// Added on 2017/05/22 to prevent irrelevant sms to add as a breakdown
+                    DBHandler dbHandler = new DBHandler(context, null, null, 1);
+                    dbHandler.addBreakdown(sID, time, sAcct_num, sFullMessage, sJob_No, sPhone_No, sAddress);
+                    dbHandler.close();
                 }
             }
             while (cursor.moveToNext());
-        }
-        else
-        {
+        } else {
             // empty box, no SMS
         }
         progress.dismiss();
-
-
         cursor.close();
+    }
+
+    private static boolean IsValidJobNo(String jobNo) {
+        if (jobNo == null) return false;
+        return jobNo.length() >= 20;
     }
 
     static String extractAccountNo(String sInputText)//To detect area codes part (41, 42 ) in the account no
     {
-        String sAccountNo="";
-        String ProbableAreaCodesMask ="";
+        String sAccountNo = "";
+        String ProbableAreaCodesMask = "";
 
-        try{
-            if (Globals.NoOfAreaCodes==1 && Globals.AreaCode1.length()==2) //detect only area code 41
+        try {
+            if (Globals.NoOfAreaCodes == 1 && Globals.AreaCode1.length() == 2) //detect only area code 41
             {
-                ProbableAreaCodesMask ="(" + Globals.AreaCode1 + "\\d\\d\\d\\d\\d\\d\\d\\d" + ")" ;
-            }
-            else if (Globals.NoOfAreaCodes==2 && Globals.AreaCode1.length()==2 && Globals.AreaCode2.length()==2)//detect both area codes 41,42
+                ProbableAreaCodesMask = "(" + Globals.AreaCode1 + "\\d\\d\\d\\d\\d\\d\\d\\d" + ")";
+            } else if (Globals.NoOfAreaCodes == 2 && Globals.AreaCode1.length() == 2 && Globals.AreaCode2.length() == 2)//detect both area codes 41,42
             {
-                ProbableAreaCodesMask ="(" + Globals.AreaCode1 + "\\d\\d\\d\\d\\d\\d\\d\\d" +  "|"
-                        + Globals.AreaCode2 + "\\d\\d\\d\\d\\d\\d\\d\\d" + ")" ;
-            }
-            else if (Globals.NoOfAreaCodes==3 && Globals.AreaCode1.length()==2
-                    && Globals.AreaCode2.length()==2 && Globals.AreaCode3.length()==2)//detect all the area codes 41,42,71
+                ProbableAreaCodesMask = "(" + Globals.AreaCode1 + "\\d\\d\\d\\d\\d\\d\\d\\d" + "|"
+                        + Globals.AreaCode2 + "\\d\\d\\d\\d\\d\\d\\d\\d" + ")";
+            } else if (Globals.NoOfAreaCodes == 3 && Globals.AreaCode1.length() == 2
+                    && Globals.AreaCode2.length() == 2 && Globals.AreaCode3.length() == 2)//detect all the area codes 41,42,71
             {
-                ProbableAreaCodesMask ="(" + Globals.AreaCode1 + "\\d\\d\\d\\d\\d\\d\\d\\d" + "|"
-                        + Globals.AreaCode2 + "\\d\\d\\d\\d\\d\\d\\d\\d" + "|" + Globals.AreaCode3 + "\\d\\d\\d\\d\\d\\d\\d\\d" + ")" ;
+                ProbableAreaCodesMask = "(" + Globals.AreaCode1 + "\\d\\d\\d\\d\\d\\d\\d\\d" + "|"
+                        + Globals.AreaCode2 + "\\d\\d\\d\\d\\d\\d\\d\\d" + "|" + Globals.AreaCode3 + "\\d\\d\\d\\d\\d\\d\\d\\d" + ")";
             }
 
-            String patternString=  ProbableAreaCodesMask;
+            String patternString = ProbableAreaCodesMask;
             Matcher m = Pattern.compile(patternString).matcher(sInputText);
 
-            if(m.find())
-            {
+            if (m.find()) {
                 sAccountNo = m.group(1);
             }
-        }catch(Exception e){
-            Log.e("extractAccountNo",e.getMessage());
-            sAccountNo="0000000000";
+        } catch (Exception e) {
+            Log.e("extractAccountNo", e.getMessage());
+            sAccountNo = "0000000000";
         }
         return sAccountNo;
     }
@@ -132,50 +120,46 @@ public class ReadSMS
     //Pattern J(41|71|77)/[A-Z]/2\d\d\d/\d\d/\d\d/\d*\.\d*
     public static String extractJobNo(String sInputText)//To detect Job no
     {
-        String sJobNo="";
-        String ProbableAreaCodesMask ="";
-        try{
-        if (Globals.NoOfAreaCodes==1 && Globals.AreaCode1.length()==2) //detect only area code 41
-        {
-            ProbableAreaCodesMask ="(J" + "(" + Globals.AreaCode1 + ")" + "/[A-Z]/2\\d\\d\\d/\\d\\d/\\d\\d/\\d*\\.\\d*)";
-        }
-        else if (Globals.NoOfAreaCodes==2 && Globals.AreaCode1.length()==2 && Globals.AreaCode2.length()==2)//detect both area codes 41,42
-        {
-            ProbableAreaCodesMask ="(J(" + Globals.AreaCode1 + "|" + Globals.AreaCode2 + ")" + "/[A-Z]/2\\d\\d\\d/\\d\\d/\\d\\d/\\d*\\.\\d*)" ;
-        }
-        else if (Globals.NoOfAreaCodes==3 && Globals.AreaCode1.length()==2
-                && Globals.AreaCode2.length()==2 && Globals.AreaCode3.length()==2)//detect all the area codes 41,42,71
-        {
-            ProbableAreaCodesMask ="(J(" + Globals.AreaCode1 + "|" + Globals.AreaCode2 + "|" + Globals.AreaCode3 + ")"
-                    + "/[A-Z]/2\\d\\d\\d/\\d\\d/\\d\\d/\\d*\\.\\d*)" ;
-        }
+        String sJobNo = "";
+        String ProbableAreaCodesMask = "";
+        try {
+            if (Globals.NoOfAreaCodes == 1 && Globals.AreaCode1.length() == 2) //detect only area code 41
+            {
+                ProbableAreaCodesMask = "(J" + "(" + Globals.AreaCode1 + ")" + "/[A-Z]/2\\d\\d\\d/\\d\\d/\\d\\d/\\d*\\.\\d*)";
+            } else if (Globals.NoOfAreaCodes == 2 && Globals.AreaCode1.length() == 2 && Globals.AreaCode2.length() == 2)//detect both area codes 41,42
+            {
+                ProbableAreaCodesMask = "(J(" + Globals.AreaCode1 + "|" + Globals.AreaCode2 + ")" + "/[A-Z]/2\\d\\d\\d/\\d\\d/\\d\\d/\\d*\\.\\d*)";
+            } else if (Globals.NoOfAreaCodes == 3 && Globals.AreaCode1.length() == 2
+                    && Globals.AreaCode2.length() == 2 && Globals.AreaCode3.length() == 2)//detect all the area codes 41,42,71
+            {
+                ProbableAreaCodesMask = "(J(" + Globals.AreaCode1 + "|" + Globals.AreaCode2 + "|" + Globals.AreaCode3 + ")"
+                        + "/[A-Z]/2\\d\\d\\d/\\d\\d/\\d\\d/\\d*\\.\\d*)";
+            }
 
-        String patternString=  ProbableAreaCodesMask;
-        Matcher m = Pattern.compile(patternString).matcher(sInputText);
+            String patternString = ProbableAreaCodesMask;
+            Matcher m = Pattern.compile(patternString).matcher(sInputText);
 
-        if(m.find())
-        {
-            sJobNo = m.group(1);
-        }
-        }catch(Exception e){
-            Log.e("extractJobNo",e.getMessage());
-            sJobNo="0000000000";
+            if (m.find()) {
+                sJobNo = m.group(1);
+            }
+        } catch (Exception e) {
+            Log.e("extractJobNo", e.getMessage());
+            sJobNo = "0000000000";
         }
         return sJobNo;
     }
 
     public static String extractPhoneNo(String sInputText)//To detect Phone no
     {
-        String sPhoneNo="";
-        String ProbableAreaCodesMask ="";
+        String sPhoneNo = "";
+        String ProbableAreaCodesMask = "";
 
-        ProbableAreaCodesMask ="(\\b0\\d\\d\\d\\d\\d\\d\\d\\d\\d\\b|\\b\\d\\d\\d\\d\\d\\d\\d\\d\\d\\b)" ;
+        ProbableAreaCodesMask = "(\\b0\\d\\d\\d\\d\\d\\d\\d\\d\\d\\b|\\b\\d\\d\\d\\d\\d\\d\\d\\d\\d\\b)";
 
-        String patternString=  ProbableAreaCodesMask;
+        String patternString = ProbableAreaCodesMask;
         Matcher m = Pattern.compile(patternString).matcher(sInputText);
 
-        if(m.find())
-        {
+        if (m.find()) {
             sPhoneNo = m.group(1);
         }
         return sPhoneNo;
